e9cc0e0e902bd2a97d45c2c4407f0aad997bf92f
mmohiesen96 <mmohiesen996@gmail.com>
2021-06-28 22:26:40 +0300

    insertShiftArray challenge Done

---
+++ challenges/arrInsertShift/insert-shift-array.java
@@ -0,0 +1,29 @@
+package com.company.averagefinder;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    static int[] insertShiftArray (int[] myArr , int newItem) {
+        int mid;
+        int newArr[] = new int[myArr.length + 1];
+        mid = myArr.length % 2 == 0 ?  mid = myArr.length / 2 : myArr.length / 2 + 1 ;
+
+        for (int i = 0; i <newArr.length ; i++) {
+
+            if (i < mid) {
+                newArr[i] = myArr[i];
+            }
+            else if (i == mid) {
+                newArr[i] = newItem;
+            }
+            else {
+                newArr[i] = myArr[i-1];
+            }
+        }
+        return newArr;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(insertShiftArray(x,5)));
+    }
+}
