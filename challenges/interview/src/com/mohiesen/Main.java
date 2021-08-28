1df588a3eeae15874f1d2935a1fe8c43def8c12f
mmohiesen96 <mmohiesen996@gmail.com>
2021-08-28 13:12:15 +0300

    done

--- challenges/interview/src/com/mohiesen/Main.java
+++ challenges/interview/src/com/mohiesen/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
     public static boolean isPrime(int num){
         if(num > 2 && num >=0) {
-            for (int i = 2; i < num-1; i++) {
+            for (int i = 2; i < Math.sqrt(num); i++) {
                 if(num % i == 0){
                     return false;
                 }
@@ -16,10 +16,20 @@ public class Main {
     public static void main(String[] args) {
         int n = 3;
         int[] myArr = {1 , 2 , 3};
+//        {1,2,3,1,2,3}
         int[] mySecondArr = new int[n*2];
         for (int i = 0; i < myArr.length; i++) {
             mySecondArr[i] = myArr[i];
             mySecondArr[i + myArr.length] = myArr[i];
+        }
+
+        try{
+            System.out.println("maryam");
+            throw new NullPointerException();
+        }
+
+        finally {
+            System.out.println("Hello");
         }
 
 
@@ -33,7 +43,7 @@ In package moh2 Class c
 In class a I have method f1
 Class c extends class a
 and b extends class a
-protected
+protected to package
  */
 
 /*
--- challenges/sketches/src/com/company/Main.java
+++ challenges/sketches/src/com/company/Main.java
@@ -5,7 +5,17 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-
+    public static boolean isUnique(String sentence){
+        HashSet<Character> unique = new HashSet<>();
+        sentence = sentence.replace(" " , "");
+        for (int i = 0; i < sentence.length(); i++) {
+            if (unique.add(sentence.charAt(i))){
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
     public static void main(String[] args) {
 //        for (int i = 0; i < 5; i++) {
 //            for (int j = 0; j <= i; j++) {
@@ -24,14 +34,7 @@ public class Main {
 //            System.out.println();
 //        }
 //
-        HashSet<Character> unique = new HashSet<>();
-        Scanner sc = new Scanner(System.in);
-        String myString = sc.nextLine();
-        myString = myString.replace(" " , "");
-        for (int i = 0; i < myString.length(); i++) {
-            unique.add(myString.charAt(i));
-        }
 
-        System.out.println(unique.size() != myString.length());
+        System.out.println(isUnique("helo"));
     }
 }
